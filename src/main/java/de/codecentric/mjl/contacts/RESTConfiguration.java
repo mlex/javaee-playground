acad96680e5fa1146321f5c6cd227a4863008178
Michael Lex <mich.lex@gmail.com>
2014-03-14 09:05:32 +0100

    Make rest-resources available under /rest/contacts.

--- src/main/java/de/codecentric/mjl/contacts/RESTConfiguration.java
+++ src/main/java/de/codecentric/mjl/contacts/RESTConfiguration.java
@@ -3,6 +3,6 @@ package de.codecentric.mjl.contacts;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("/contacts")
+@ApplicationPath("/rest")
 public class RESTConfiguration extends Application {
 }
